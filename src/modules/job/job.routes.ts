da14ba299b1442d1513cc755849574bb96e88906
0xprathamesh <prathameshpatil6499@gmail.com>
2026-03-15 15:51:48 +0530

    feat:docs

--- src/modules/job/job.routes.ts
+++ src/modules/job/job.routes.ts
@@ -3,7 +3,7 @@ import { jobController } from "./job.controller"
 
 const router = Router()
 
-router.post("/job", (req, res) => void jobController.createJob(req, res))
-router.get("/job/:id", (req, res) => void jobController.getJob(req, res))
+router.post("/", (req, res) => void jobController.createJob(req, res))
+router.get("/:id", (req, res) => void jobController.getJob(req, res))
 
 export default router
